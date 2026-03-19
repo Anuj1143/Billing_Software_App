174fa95a8e07db8f6fd93d7c4b2e477d7c5b2d1a
anujm <aanujmaurya8127@gmail.com>
2026-03-19 16:29:26 +0530

    Write all the end Points related to category Class

---
+++ billingsoftware/src/main/java/com/billing/controller/CategoryController.java
@@ -0,0 +1,38 @@
+package com.billing.controller;
+
+import com.billing.io.CategoryRequest;
+import com.billing.io.CategoryResponse;
+import com.billing.service.CategoryService;
+import com.billing.service.impl.CategoryServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryResponse addCategory(@RequestBody CategoryRequest request){
+       return categoryService.add(request);
+    }
+    @GetMapping
+    public List<CategoryResponse> fetchCategories(){
+        return categoryService.read();
+    }
+    @DeleteMapping("/{categoryId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remove(@PathVariable String categoryId){
+        try{
+                categoryService.delete(categoryId);
+        }catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+}
