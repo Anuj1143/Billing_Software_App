bed9d484f671e3eacacbd0baf1b1a50f4c9d7e34
anujm <aanujmaurya8127@gmail.com>
2026-03-19 16:31:26 +0530

    this is category service class and interface where all the methods are define

---
+++ billingsoftware/src/main/java/com/billing/service/CategoryService.java
@@ -0,0 +1,13 @@
+package com.billing.service;
+
+import com.billing.io.CategoryRequest;
+import com.billing.io.CategoryResponse;
+
+import java.util.List;
+
+public interface CategoryService {
+
+  CategoryResponse add(CategoryRequest request);
+  List<CategoryResponse> read();
+  void delete (String categoryId);
+}
