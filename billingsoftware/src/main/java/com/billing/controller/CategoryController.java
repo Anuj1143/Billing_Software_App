c3c2d8799547e4348ef870d5fd6c2407a8d2ebe8
anujm <aanujmaurya8127@gmail.com>
2026-03-21 11:00:29 +0530

    Update addCategory end point for store the image

--- billingsoftware/src/main/java/com/billing/controller/CategoryController.java
+++ billingsoftware/src/main/java/com/billing/controller/CategoryController.java
@@ -7,7 +7,10 @@ import com.billing.service.impl.CategoryServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+import software.amazon.awssdk.thirdparty.jackson.core.JsonProcessingException;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
@@ -19,8 +22,12 @@ public class CategoryController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CategoryResponse addCategory(@RequestBody CategoryRequest request){
-       return categoryService.add(request);
+    public CategoryResponse addCategory(@RequestPart("category") String categoryString, @RequestPart("file") MultipartFile file) throws JsonProcessingException {
+        ObjectMapper objectMapper=new ObjectMapper();
+        CategoryRequest request=null;
+        request=  objectMapper.readValue(categoryString, CategoryRequest.class);
+        return categoryService.add(request,file);
+
     }
     @GetMapping
     public List<CategoryResponse> fetchCategories(){
